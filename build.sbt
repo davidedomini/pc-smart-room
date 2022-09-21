52c9d1a636be6b42a4d9abd3219f324af92521ed
dom <davidedomini@gmail.com>
2022-09-21 22:59:21 +0200

    implemented a first version of light agent

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "pc-smart-room"
 version := "0.1"
 
 scalaVersion := "3.1.3"
+
+// https://mvnrepository.com/artifact/io.vertx/vertx-core
+libraryDependencies += "io.vertx" % "vertx-core" % "4.3.3"
+libraryDependencies += "io.vertx" % "vertx-mqtt" % "4.3.3"
--- src/main/scala/it/unibo/pc/smart_room/decentralized/light/LightAgent.scala
+++ src/main/scala/it/unibo/pc/smart_room/decentralized/light/LightAgent.scala
@@ -1,3 +1,31 @@
 package it.unibo.pc.smart_room.decentralized.light
 
-class LightAgent {}
+import io.vertx.core.{AbstractVerticle, Promise, Vertx}
+import io.vertx.mqtt.MqttClient
+import io.netty.handler.codec.mqtt.MqttQoS
+import io.vertx.core.buffer.Buffer
+
+class LightAgent extends AbstractVerticle:
+
+  override def start(): Unit =
+    val client: MqttClient = MqttClient.create(vertx)
+    client.connect(
+      1883,
+      "broker.mqtt-dashboard.com",
+      c =>
+        println("Connected...")
+        client
+          .publishHandler(s =>
+            println(s"There is a new message in the topic ${s.topicName}")
+            println(s"Content of the message: ${s.payload().toString()}")
+            println(s"QoS: ${s.qosLevel}")
+          )
+          .subscribe("light", 2)
+
+      //client.publish("light", Buffer.buffer("hello"), MqttQoS.AT_LEAST_ONCE, false, false);
+    )
+
+object TestAgent extends App:
+  val vertx: Vertx = Vertx.vertx
+  val agent = new LightAgent
+  vertx.deployVerticle(agent)
